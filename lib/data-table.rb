778fddfcaa389e6d2e5bd41796b1acf73c327d3f
aharpervc <aharper@breuer.com>
2017-03-31 13:57:04 -0400

    Styles: removed empty css rules

--- lib/data-table.rb
+++ lib/data-table.rb
@@ -26,18 +26,14 @@ module DataTable
 
       .data_table caption {font-size: 2em; font-weight: bold}
 
-      .data_table thead {}
       .data_table thead th {background-color: #ddd; border-bottom: 1px solid #bbb;}
 
-      .data_table tbody {}
       .data_table tbody tr.alt {background-color: #eee;}
 
       .data_table .group_header th {text-align: left;}
 
-      .data_table .subtotal {}
       .data_table .subtotal td {border-top: 1px solid #000;}
 
-      .data_table tfoot {}
       .data_table tfoot td {border-top: 1px solid #000;}
 
       .empty_data_table {text-align: center; background-color: #ffc;}
